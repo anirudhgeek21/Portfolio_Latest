c4a2e03ff7c2c2494bf9156e7a57ec22c97325f7
Anirudh Prajapati <81011908+anirudhgeek21@users.noreply.github.com>
2024-02-27 20:43:47 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(cors({
     origin: 'https://anirudh-prajapati.vercel.app',
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["POST"],
     credentials: true
   }));
 
